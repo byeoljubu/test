8c2ab256a441b77b55547a5ac8151d4fb973d298
Han seungjin <suku10@kangwon.ac.kr>
2018-10-02 13:38:42 +0900

    test 2

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,6 +2,6 @@ public class Hello {
 
     public static void main(String[] args){
         System.out.println("Hello World");
-    	System.out.println("Make a new branch");
+    	System.out.println("Second Test");
     }
 }
